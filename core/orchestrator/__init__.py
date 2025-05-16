18654c988cd43b3bf010be26110d54f9ab311cfd
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-16 17:16:51 -0400

    Create __init__.py

---
+++ core/orchestrator/__init__.py
@@ -0,0 +1,6 @@
+# ===================================
+# 📁 core/orchestrator/__init__.py
+# ===================================
+from .main_orchestrator import Orchestrator
+
+__all__ = ["Orchestrator"]
