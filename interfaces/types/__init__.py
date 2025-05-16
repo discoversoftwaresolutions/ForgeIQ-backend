403393ce61de187138243ce4900841dac79e9218
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-16 12:09:47 -0400

    Update __init__.py

--- interfaces/types/__init__.py
+++ interfaces/types/__init__.py
@@ -1,1 +1,23 @@
 
+# interfaces/types/__init__.py
+from .events import (
+    TestResult, TestFailedEvent,
+    CodeNavSearchQuery, CodeNavSearchResultItem, CodeNavSearchResults,
+    PatchSuggestion, PatchSuggestedEvent,
+    PipelineGenerationRequest, DagNode, DagDefinition, DagDefinitionCreatedEvent,
+    TaskStatus, TaskStatusUpdateEvent, DagExecutionStatusEvent
+)
+from .cache import ( # <<< NEW
+    CacheKeyParams, CachedItemMetadata, 
+    CacheGetResponse, CacheStoreRequest, CacheStoreResponse
+)
+
+__all__ = [
+    "TestResult", "TestFailedEvent",
+    "CodeNavSearchQuery", "CodeNavSearchResultItem", "CodeNavSearchResults",
+    "PatchSuggestion", "PatchSuggestedEvent",
+    "PipelineGenerationRequest", "DagNode", "DagDefinition", "DagDefinitionCreatedEvent",
+    "TaskStatus", "TaskStatusUpdateEvent", "DagExecutionStatusEvent",
+    "CacheKeyParams", "CachedItemMetadata", # <<< NEW
+    "CacheGetResponse", "CacheStoreRequest", "CacheStoreResponse" # <<< NEW
+]
