575338f62422186b20fc264b2c18e2da7f091b2c
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-16 11:13:14 -0400

    Create __init__.py

---
+++ interfaces_python/__init__.py
@@ -0,0 +1,9 @@
+# ... (existing exports) ...
+from .events import (
+    PipelineGenerationRequest, DagNode, DagDefinition, DagDefinitionCreatedEvent
+)
+
+__all__ = [
+    # ... (existing exports) ...
+    "PipelineGenerationRequest", "DagNode", "DagDefinition", "DagDefinitionCreatedEvent"
+]
