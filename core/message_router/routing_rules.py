d74f57db91c8b6cda3df1feab21e4cdf922e4e0e
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-18 10:17:51 -0400

    Create routing_rules.py

---
+++ core/message_router/routing_rules.py
@@ -0,0 +1,9 @@
+ROUTING_RULES = {
+    "agent_status_update": "agent_manager.handle_status_update",
+    "deployment_request": "deployment_manager.process_request",
+    "cache_lookup": "cache_service.fetch_item",
+}
+
+def get_routing_target(route_name: str) -> Optional[str]:
+    """Returns the target function for a given route, if defined."""
+    return ROUTING_RULES.get(route_name)
